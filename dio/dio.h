77efec22c4d62ac7058df69fd6103724ac1a3d7f
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2018-12-14 12:00:35 -0500

    updated with more specific names

--- dio/dio.h
+++ dio/dio.h
@@ -13,10 +13,10 @@ enum PinMode
 	ModeOutput
 };
 
-extern "C" bool init(const char* initFile);
+extern "C" bool initDio(const char* initFile);
 extern "C" int digitalRead(int dio, int pin);
 extern "C" int digitalWrite(int dio, int pin, bool state);
 extern "C" int setOutputMode(int dio, OutputMode mode);
-extern "C" const char* getLastError();
+extern "C" const char* getLastDioError();
 
 #endif
--- poe/poe.h
+++ poe/poe.h
@@ -9,9 +9,9 @@ enum PoeState
     StateError          //Error retreiving state. Use getLastError and getLastErrorString for more details.
 };
 
-extern "C" bool init(const char *initFile);
+extern "C" bool initPoe(const char *initFile);
 extern "C" PoeState getPortState(int port);
 extern "C" int setPortState(int port, PoeState state);
-extern "C" const char* getLastError();
+extern "C" const char* getLastPoeError();
 
 #endif
