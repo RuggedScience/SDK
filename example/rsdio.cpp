e1d1fa4d48e3fa73d8262e71e4bc1d5640e5af10
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2019-01-28 16:04:24 -0500

    added command line options removed interactive mode

--- example/rsdio.cpp
+++ example/rsdio.cpp
@@ -1,76 +1,148 @@
 #include "../dio/rsdio.h"
 #include <iostream>
 #include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+static int stringToState(std::string str)
+{
+	std::transform(str.begin(), str.end(), str.begin(), toupper);
+	if (str == "DISABLED" || str == "FALSE")
+		return 0;
+	else if (str == "ENABLED" || str == "TRUE")
+		return 1;
+	
+	return -1;
+}
+
+static const char *stateToString(bool state)
+{
+	if (!state)
+		return "disabled";
+	else
+		return "enabled";
+}
 
 void printLastError()
 {
 	printf("Error: %s\n", getLastDioError());
 }
 
-//This function goes into a loop to allow interactive control over the DIO
-void interactiveDio()
+static void showUsage()
 {
-	if (setOutputMode(1, ModeNpn) < 0) 
-		printLastError();
-	if (setOutputMode(2, ModePnp) < 0) 
-		printLastError();
-
-	char cmd;
-	int dio, pin, state;
-	while (1)
-	{
-		printf("Please enter a command\n");
-		printf("r = Read pin\n");
-		printf("s = Set pin\n");
-		printf("e = Exit\n");
-		std::cin >> cmd;
-		std::cin.clear();
-		if (cmd == 'e') break;
-
-		printf("Please enter a dio number\n");
-		std::cin >> dio;
-		std::cin.clear();
-
-		printf("Please enter a pin number\n");
-		std::cin >> pin;
-		std::cin.clear();
-
-		if (cmd == 'r')
-		{
-			state = digitalRead(dio, pin);
-			if (state >= 0)
-				printf("Pin %d is %s\n", pin, state ? "high" : "low");
-			else 
-				printLastError();
-		}
-		else if (cmd == 's')
-		{
-			printf("Enter desired state\n");
-			printf("0 = LOW\n");
-			printf("1 = HIGH\n");
-			std::cin >> state;
-			std::cin.clear();
-
-			if (digitalWrite(dio, pin, state) < 0) 
-				printLastError();
-		}
-		else 
-			printf("Invalid command!!!\n");
-	}
+	std::cout 	<< "Usage: rsdio FILE COMMAND DIO PIN [OPTIONS]...\n"
+				<< "Commands:\n"
+				<< "s, state\t\toutput the state of a pin\n"
+				<< "\t\t\trequires DIO and PIN to be defined\n"
+				<< "s, state=STATE\t\tsets the state of a pin\n"
+				<< "\t\t\tStates:\n"
+				<< "\t\t\t0, DISABLED\n"
+				<< "\t\t\t1, ENABLED\n"
+				<< "\t\t\trequires DIO and PIN to be defined\n"
+				<< "help\t\t\tdisplay this help and exit\n"
+				<< "Options:\n"
+				<< "-h, --human-readable \toutput data in a human readable format\n";
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	// Create a list of args without optional switches
+	// Allows for switches to be position independent
+	bool human = false;
+	std::vector<std::string> argList;
+	for (int i = 1; i < argc; ++i)
 	{
-		printf("Provide an xml file\n");
+		std::string arg = argv[i];
+		if (arg == "help")
+		{
+			showUsage();
+			return 0;
+		}
+		else if (arg == "-h" || arg == "--human-readable")
+			human = true;
+		else
+			argList.emplace_back(arg);
+	}
+
+	if (argList.size() < 4)
+	{
+		showUsage();
 		return 1;
 	}
 
-	if (initDio(argv[1]))
-		interactiveDio();
-	else
-		std::cout << getLastDioError() << std::endl;
+	if (!initDio(argList[0].data()))
+	{
+		printLastError();
+		return 1;
+	}
+
+	int dio, pin;
+	try { dio = std::stoi(argList[2]); }
+	catch (...)
+	{
+		std::cerr << "DIO must be a number!!" << std::endl;
+		showUsage();
+		return 1;
+	}
+
+	try { pin = std::stoi(argList[3]); }
+	catch (...)
+	{
+		std::cerr << "PIN must be a number!!" << std::endl;
+		showUsage();
+		return 1;
+	}
+
+	// If the command is "state=" we need to break it up into command / value.
+	int state;
+	std::string& cmd = argList[1];	
+	size_t index = cmd.find("=");
+	if (index != cmd.npos)
+	{
+		std::string val = cmd.substr(index + 1);
+		cmd = cmd.substr(0, cmd.size() - val.size());
+		state = stringToState(val);
+
+		// stringToState returs -1 if it couldn't convert it.
+		// Let's check if they used a number instead of string.
+		if (state == -1)
+		{
+			try { state = std::stoi(val); }
+			catch (...) {}
+		}
+
+		// Couldn't convert the state from a string or an int... give up.
+		if (state == -1)
+		{
+			std::cerr << "Invalid state supplied!!" << std::endl;
+			showUsage();
+			return 1;
+		}
+	}
+
+	if (cmd == "s=" || cmd == "state=")
+	{
+		if (digitalWrite(dio, pin, state) < 0)
+		{
+			printLastError();
+			return 1;
+		}
+	}
+	else if (cmd == "s" || cmd == "state")
+	{
+		state = digitalRead(dio, pin);
+		if (state >= 0)
+		{
+			if (human) printf("%s\n", stateToString(state));
+			else printf("%d", state);
+		}
+		else
+		{
+			printLastError();
+			return 1;
+		}
+	}
 
 	return 0;
 }
