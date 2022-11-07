262ed2f4352048e1af9c1fd393030812a4e9fcbf
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2022-11-07 14:03:57 -0800

    Package driver DLL with Python module on windows

--- extras/python-bindings/src/rssdk/__init__.py
+++ extras/python-bindings/src/rssdk/__init__.py
@@ -1,4 +1,12 @@
 __all__ = ['RsDio', 'OutputMode', 'RsPoe', 'PoeState']
 
+import os
+
+# On Windows we will install the driver DLL with this module.
+# Make sure we tell Windows to look here.
+if os.name == 'nt':
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.add_dll_directory(dir_path)
+
 from .rsdio import RsDio, OutputMode
 from .rspoe import RsPoe, PoeState
