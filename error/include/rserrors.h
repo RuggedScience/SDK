6e01f2d573ae84a5c285d5b12ade6d2a627f649a
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2022-12-05 10:52:44 -0800

    Moved category implemention to source file

--- error/include/rserrors.h
+++ error/include/rserrors.h
@@ -33,80 +33,16 @@ namespace detail
 class RsErrorCodeCategory : public std::error_category
 {
 public:
-    const char *name() const noexcept override final
-    {
-        return "RsErrorCode";
-    }
-
-    std::string message(int c) const override final
-    {
-        switch (static_cast<RsErrorCode>(c))
-        {
-        case RsErrorCode::NotInitialized:
-            return "Object not initialized";
-        case RsErrorCode::XmlParseError:
-            return "XML parsing error";
-        default:
-            return "Unknown Error";
-        }
-    }
+    const char *name() const noexcept override final;
+    std::string message(int c) const override final;
 };
 
 class RsErrorConditionCategory : public std::error_category
 {
 public:
-    const char *name() const noexcept override final
-    {
-        return "RsErrorCondition";
-    }
-
-    std::string message(int c) const override final
-    {
-        switch (static_cast<RsErrorCondition>(c))
-        {
-        case RsErrorCondition::HardwareError:
-            return "Hardware error";
-        case RsErrorCondition::UnsupportedFunction:
-            return "Unsupported Function";
-        case RsErrorCondition::PermissionErrror:
-            return "Permission error";
-        case RsErrorCondition::InitializationError:
-            return "Initialization error";
-        default:
-            return "Unknown Error";
-        }
-    }
-
-    bool equivalent(const std::error_code& ec, int cond) const noexcept override final
-    {
-        switch (static_cast<RsErrorCondition>(cond))
-        {
-            case RsErrorCondition::HardwareError:
-                return (
-                    ec == std::errc::device_or_resource_busy ||
-                    ec == std::errc::no_such_device || 
-                    ec == std::errc::protocol_error
-                );
-            case RsErrorCondition::UnsupportedFunction:
-                return (
-                    ec == std::errc::no_such_device ||
-                    ec == std::errc::function_not_supported ||
-                    ec == std::errc::invalid_argument
-                );
-            case RsErrorCondition::PermissionErrror:
-                return (
-                    ec == std::errc::operation_not_permitted ||
-                    ec == std::errc::permission_denied
-                );
-            case RsErrorCondition::InitializationError:
-                return (
-                    ec == RsErrorCode::NotInitialized ||
-                    ec == RsErrorCode::XmlParseError
-                );
-        }
-
-        return false;
-    }
+    const char *name() const noexcept override final;
+    std::string message(int c) const override final;
+    bool equivalent(const std::error_code& ec, int cond) const noexcept override final;
 };
 
 }
--- error/src/rserrors.cpp
+++ error/src/rserrors.cpp
@@ -2,6 +2,82 @@
 
 #include <iostream>
 
+namespace detail
+{
+    
+  const char *RsErrorCodeCategory::name() const noexcept
+  {
+    return "RsErrorCode";
+  }
+
+  std::string RsErrorCodeCategory::message(int c) const
+  {
+    switch (static_cast<RsErrorCode>(c))
+    {
+    case RsErrorCode::NotInitialized:
+      return "Object not initialized";
+    case RsErrorCode::XmlParseError:
+      return "XML parsing error";
+    default:
+      return "Unknown Error";
+    }
+  }
+
+  const char *RsErrorConditionCategory::name() const noexcept
+  {
+    return "RsErrorCondition";
+  }
+
+  std::string RsErrorConditionCategory::message(int c) const
+  {
+    switch (static_cast<RsErrorCondition>(c))
+    {
+    case RsErrorCondition::HardwareError:
+      return "Hardware error";
+    case RsErrorCondition::UnsupportedFunction:
+      return "Unsupported Function";
+    case RsErrorCondition::PermissionErrror:
+      return "Permission error";
+    case RsErrorCondition::InitializationError:
+      return "Initialization error";
+    default:
+      return "Unknown Error";
+    }
+  }
+
+  bool RsErrorConditionCategory::equivalent(const std::error_code& ec, int cond) const noexcept
+  {
+    switch (static_cast<RsErrorCondition>(cond))
+    {
+      case RsErrorCondition::HardwareError:
+        return (
+          ec == std::errc::device_or_resource_busy ||
+          ec == std::errc::no_such_device || 
+          ec == std::errc::protocol_error
+        );
+      case RsErrorCondition::UnsupportedFunction:
+        return (
+          ec == std::errc::no_such_device ||
+          ec == std::errc::function_not_supported ||
+          ec == std::errc::invalid_argument
+        );
+      case RsErrorCondition::PermissionErrror:
+        return (
+          ec == std::errc::operation_not_permitted ||
+          ec == std::errc::permission_denied
+        );
+      case RsErrorCondition::InitializationError:
+        return (
+          ec == RsErrorCode::NotInitialized ||
+          ec == RsErrorCode::XmlParseError
+        );
+    }
+
+    return false;
+}
+
+} // namespace detail
+
 const detail::RsErrorCodeCategory &errorCodeCategory()
 {
   static detail::RsErrorCodeCategory c;
