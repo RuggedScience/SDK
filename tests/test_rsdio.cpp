301400243ffc5303c1dfbec61c08ecc4c38ad82b
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2022-12-05 10:11:11 -0800

    Updated tests

--- tests/test_rsdio.cpp
+++ tests/test_rsdio.cpp
@@ -5,57 +5,51 @@
 #include <rsdio.h>
 #include <rserrors.h>
 
-#include "argparse.h"
-
 typedef std::shared_ptr<rs::RsDio> RsDio_t;
 
-int testNoSuchFile(RsDio_t dio)
+int testXml(const char *xmlFile)
 {
-    if (dio->setXmlFile("fake_file"))
+    RsDio_t dio(rs::createRsDio(), std::mem_fn(&rs::RsDio::destroy));
+    if (!dio->setXmlFile(xmlFile))
+    {
+        std::cerr << "setXmlFile returned false with valid file" << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
+int testXml(const char *xmlFile, std::error_code expectedError)
+{
+    RsDio_t dio(rs::createRsDio(), std::mem_fn(&rs::RsDio::destroy));
+    if (dio->setXmlFile(xmlFile))
     {
         std::cerr << "setXmlFile returned true with invalid file" << std::endl;
         return 1;
     }
 
-    if (dio->getLastError() != std::errc::no_such_file_or_directory)
+    if (dio->getLastError() != expectedError)
     {
-        std::cerr << "Expected no such file error. Got " << dio->getLastErrorString() << std::endl;
+        std::cerr << "Expected " << expectedError.message() << ". Got " << dio->getLastErrorString() << std::endl;
         return 1; 
     }
     return 0;
 }
 
+int testXml(const char *xmlFile, std::errc expectedError)
+{
+    return testXml(xmlFile, std::make_error_code(expectedError));
+}
+
+int testXml(const char *xmlFile, RsErrorCode expectedError)
+{
+    return testXml(xmlFile, make_error_code(expectedError));
+}
+
 int main(int argc, char *argv[])
 {
-    ArgParse mainParser("rsdio_test");
-
-    mainParser.addPositionalArg(PositionalArg("filename", "XML file to parse"));
-
-    PositionalArg commandArg("command", "Test command to run");
-    // Run all tests. We don't need any args but let the parser know about he option.
-    mainParser.addSubCommand(commandArg, "all");
-
-    // Test XML parsing
-    ArgParse &xmlCommandParser = mainParser.addSubCommand(commandArg, "xml");
-
-    if (!mainParser.parse(argc, argv))
-    {
-        std::cerr << "Error parsing arguments\n";
+    if (testXml("fake_file", std::errc::no_such_file_or_directory))
         return 1;
-    }
 
-    std::string command, xmlFile;
-    mainParser.getValue("command", command);
-    mainParser.getValue("filename", xmlFile);
-    if (command == "xml")
-    {
-        
-    }
-
-    RsDio_t dio(rs::createRsDio(), std::mem_fn(&rs::RsDio::destroy));
-    if (dio->setXmlFile(xmlFile.c_str()))
-    {
-        std::cerr << "setXmlFile returned true with invalid file" << std::endl;
+    if (testXml("invalid.xml", RsErrorCode::XmlParseError))
         return 1;
-    }
 }
--- tests/test_rspoe.cpp
+++ tests/test_rspoe.cpp
@@ -5,62 +5,54 @@
 #include <rspoe.h>
 #include <rserrors.h>
 
-#include "argparse.h"
-
 typedef std::shared_ptr<rs::RsPoe> RsPoe_t;
 
-int testNoSuchFile(RsPoe_t poe)
+int testXml(const char *xmlFile)
 {
-    if (poe->setXmlFile("fake_file"))
+    RsPoe_t poe(rs::createRsPoe(), std::mem_fn(&rs::RsPoe::destroy));
+    if (!poe->setXmlFile(xmlFile))
+    {
+        std::cerr << "setXmlFile returned false with valid file" << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
+int testXml(const char *xmlFile, std::error_code expectedError)
+{
+    RsPoe_t poe(rs::createRsPoe(), std::mem_fn(&rs::RsPoe::destroy));
+    
+    if (poe->setXmlFile(xmlFile))
     {
         std::cerr << "setXmlFile returned true with invalid file" << std::endl;
         return 1;
     }
 
-    if (poe->getLastError() != std::errc::no_such_file_or_directory)
+    if (poe->getLastError() != expectedError)
     {
-        std::cerr << "Expected no such file error. Got " << poe->getLastErrorString() << std::endl;
+        std::cerr << "Expected " << expectedError.message() << ". Got " << poe->getLastErrorString() << std::endl;
         return 1; 
     }
     return 0;
 }
 
+int testXml(const char *xmlFile, std::errc expectedError)
+{
+    return testXml(xmlFile, std::make_error_code(expectedError));
+}
+
+int testXml(const char *xmlFile, RsErrorCode expectedError)
+{
+    return testXml(xmlFile, make_error_code(expectedError));
+}
+
 int main(int argc, char *argv[])
 {
-    ArgParse mainParser("rsdio_test");
-
-    mainParser.addPositionalArg(PositionalArg("filename", "XML file to parse"));
-
-    PositionalArg commandArg("command", "Test command to run");
-    // Run all tests. We don't need any args but let the parser know about he option.
-    mainParser.addSubCommand(commandArg, "all");
-
-    // Test XML parsing
-    ArgParse &xmlCommandParser = mainParser.addSubCommand(commandArg, "xml");
-
-    if (!mainParser.parse(argc, argv))
-    {
-        std::cerr << "Error parsing arguments\n";
-        //return 1;
-    }
-
-    std::string command, xmlFile;
-    mainParser.getValue("command", command);
-    mainParser.getValue("filename", xmlFile);
-    if (command == "xml")
-    {
-        
-    }
-
-    RsPoe_t poe(rs::createRsPoe(), std::mem_fn(&rs::RsPoe::destroy));
-    if (poe->setXmlFile("/home/tlassiter/SDK/xml/ecs9000.xml"))
-    {
-        std::cerr << "setXmlFile returned true with invalid file" << std::endl;
+    if (testXml("fake_file", std::errc::no_such_file_or_directory))
         return 1;
-    }
 
-    std::cout << (poe->getLastError() == RsErrorCode::XmlParseError) << std::endl;
-    std::cout << (poe->getLastError() == RsErrorCondition::HardwareError) << std::endl;
-    std::cout << (poe->getLastError() == RsErrorCondition::UserError) << std::endl;
-    std::cout << (poe->getLastError() == std::errc::operation_not_permitted) << std::endl;
+    if (testXml("invalid.xml", RsErrorCode::XmlParseError))
+        return 1;
+
+    return 0;
 }
