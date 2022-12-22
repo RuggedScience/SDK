e429dd11e5d765dd3c2f01010d5b262edc845b19
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2022-12-22 15:09:40 -0800

    Fixed driver dll not loaded

--- extras/python/rssdk/__init__.py
+++ extras/python/rssdk/__init__.py
@@ -1,1 +1,16 @@
+import os
+
+if os.name == 'nt':
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # If add_dll_directory exists (Python >= 3.8) let's use it
+    # to tell Windows where to look for the drv.dll
+    try:
+        os.add_dll_directory(dir_path)
+    # If not, manually load the dll. Windows will always check if a dll
+    # is loaded in memory first before searching other directories.
+    except AttributeError:
+        import ctypes
+        dll = os.path.join(dir_path, 'drv.dll')
+        hllDll = ctypes.WinDLL(dll)
+
 from rssdk.rssdk import *
